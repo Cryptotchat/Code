24790153b0cc0eceb20f3de44c19915c8f6959e4
clementLambert <35558278+clementLambert@users.noreply.github.com>
2018-02-15 17:13:56 +0100

    Update PublicKey.java

--- PublicKey.java
+++ PublicKey.java
@@ -1,90 +1,82 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Random;
 
-public class PublicKey {
+public class PublicAndPrivateKey {
 
-	public static BigInteger P;
-	public static BigInteger Q;
-	public static BigInteger C;
-	public static BigInteger N;
-	public static BigInteger M;
-	public static BigInteger U;
-	public static BigInteger gcd;
+	private static BigInteger P;
+	private static BigInteger Q;
+	private static BigInteger C;
+	private static BigInteger N;
+	private static BigInteger M;
+	private static BigInteger U;
+	private static BigInteger gcd;
+	private BigInteger bi;
+	private BigInteger Min;
 
-	public PublicKey(){
-		PublicKey.P = genererPremier();
-		PublicKey.Q = genererPremier();
-		this.Substract();
+	public PublicAndPrivateKey(){
+		genererNombre();
+		trouverU();
 	}
-	
-	public void Substract(){
-		byte [] v = {1};
-		BigInteger un = new BigInteger(v);
-		PublicKey.N = P.multiply(Q);
-		PublicKey.M = P.subtract(un).multiply(Q.subtract(un));
-	}
-	
 
-	public static BigInteger genererNombre() {
-		BigInteger bi;
-		BigInteger Min;
-		if (P.compareTo(Q) <= 0)
-			Min = Q;
-		else
-			Min = P;
-		
+	/* -------------------- GENERATION CLÉ PUBLIQUE ------------------------------------------------- */
+	public void genererNombre() {
+
 		do {
-		bi = new BigInteger(8, new Random());
-		bi = bi.mod(M);
-		System.out.println(bi);
-		gcd = M.gcd(bi);
+			P = genererPremier();
+			Q = genererPremier();
+			N = P.multiply(Q);
+			M = P.subtract(new BigInteger("1")).multiply(Q.subtract(new BigInteger("1")));
+
+			if (P.compareTo(Q) <= 0)
+				Min = Q;
+			else
+				Min = P;
+
+			bi = new BigInteger(8, new Random());
+			C = bi.mod(M);
+			System.out.println(C);
+			gcd = M.gcd(C);
+
 		}while (gcd.compareTo(new BigInteger("1")) != 0 || bi.compareTo(Min)<=0 || bi.compareTo(M)>=0);
-		return bi;
-	}
-	
-	public static BigInteger C() {
-		return C=genererNombre();
-	}
-	
-	public static BigInteger genererPremier() {
-
-		      // create a BigInteger object
-		      BigInteger bi;
-
-		      // create and assign value to bitLength
-		      int bitLength = 8;
-
-		      // create a random object
-		      Random rnd = new Random();
-
-		      // assign probablePrime result to bi using bitLength and rnd
-		      // static method is called using class name
-		      bi = BigInteger.probablePrime(bitLength, rnd);
-
-		      //String str = "ProbablePrime of bitlength " + bitLength + " is " +bi;
-
-		      // print bi value
-		      //System.out.println( str );
-		      return bi;
-	}
-	
-	public static BigInteger chiffrer() {
-		BigInteger ch1 = new BigInteger("66");
-		BigInteger ch2;
-		ch2 = ch1.modPow(C, N);
 		
-		return ch2;
+		// Création du fichier texte pour le programme
+		File fichierTexte = new File ("D:\\Clément\\Desktop\\PublicKey.txt");
+		// Création de "l'écrivain"
+		FileWriter ecrireFichier;
+		
+		try{
+			// Instanciation de l'objet ecrireFichier qui va écrire dans fichierTexte.txt
+			ecrireFichier = new FileWriter(fichierTexte);
+			// Écriture d'une chaîne de caractères dans le fichier texte
+			String A = N.toString();
+			String B = C.toString();
+			ecrireFichier.write(A+"\r\n"+B);
+			// "Fermeture" du FileWriter
+			ecrireFichier.close();
+		}catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
 	}
 
-	public static BigInteger dechiffrer() {
-		BigInteger de1 = chiffrer();
-		BigInteger de2;
-		de2 = de1.modPow(trouverU(),N);
-		
-		return de2;
+	public BigInteger genererPremier() {
+
+		BigInteger p1;
+		int bitLength = 8;
+		Random rnd = new Random();
+		p1 = BigInteger.probablePrime(bitLength, rnd);
+		return p1;
 	}
-	
-	public static BigInteger trouverU() {
+
+	/* -------------------- GENERATION CLÉ PRIVÉE ------------------------------------------------- */
+	public void trouverU() {
 		BigInteger x = new BigInteger("0");
 		BigInteger y = new BigInteger("1");
 		BigInteger m = M;
@@ -93,33 +85,73 @@ public class PublicKey {
 		BigInteger lastx = new BigInteger("1");
 		BigInteger lasty = new BigInteger("0");
 		BigInteger temp;
-        do{
-            BigInteger q = c.divide(n);
-            BigInteger r = c.remainder(n);
- 
-            c = n;
-            n = r;
- 
-            temp = x;
-            x = lastx.subtract(q.multiply(x));
-            lastx = temp;
- 
-            temp = y;
-            y = lasty.subtract(q.multiply(y));
-            lasty = temp;            
-        
-        }while (n.compareTo(new BigInteger("0"))>0);
-       
-        if (lastx.compareTo(new BigInteger("0"))<=0) {
-        	lastx = lastx.subtract(m.multiply(new BigInteger("-1")));
-        }
-		return lastx;
+		do{
+			BigInteger q = c.divide(n);
+			BigInteger r = c.remainder(n);
+
+			c = n;
+			n = r;
+
+			temp = x;
+			x = lastx.subtract(q.multiply(x));
+			lastx = temp;
+
+			temp = y;
+			y = lasty.subtract(q.multiply(y));
+			lasty = temp;            
+
+		}while (n.compareTo(new BigInteger("0"))>0);
+
+		if (lastx.compareTo(new BigInteger("0"))<=0) {
+			lastx = lastx.subtract(m.multiply(new BigInteger("-1")));
+		}
+		U = lastx;
+		TEST();
+		// Création du fichier texte pour le programme
+				File fichierTexte = new File ("D:\\Clément\\Desktop\\PrivateKey.txt");
+				// Création de "l'écrivain"
+				FileWriter ecrireFichier;
+				
+				try{
+					// Instanciation de l'objet ecrireFichier qui va écrire dans fichierTexte.txt
+					ecrireFichier = new FileWriter(fichierTexte);
+					// Écriture d'une chaîne de caractères dans le fichier texte
+					String A = N.toString();
+					String B = U.toString();
+					ecrireFichier.write(A+"\r\n"+B);
+					// "Fermeture" du FileWriter
+					ecrireFichier.close();
+				}catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
 	}
+
+	/* -------------------- TEST DU PROGRAMME ------------------------------------------------- */
+	public void TEST() {
+		BigInteger BASE = new BigInteger("66");
+		BigInteger RES;
+		BigInteger RES2;
+
+		RES = BASE.modPow(C, N);
+		System.out.println("ch2 = "+RES);
+		RES2 = RES.modPow(U,N);
+		System.out.println("de2 = " + RES2);
+		if (RES2.compareTo(new BigInteger("66")) != 0) {
+			genererNombre();
+			genererPremier();
+			trouverU();
+		}	
+	}
+
 	
+
+	/* -------------------- ToString ------------------------------------------------- */
 	public String toString(){
-		return "P = " + PublicKey.P + " \nQ = " + PublicKey.Q+" \nM = "+PublicKey.M+" \nClé publique = ("+PublicKey.N+","+C()+")\nPGCD = "+PublicKey.gcd +" \nClé privé = ("+PublicKey.N+","+trouverU()+")\n";
+		return "P = " + P + " \nQ = " + Q+" \nM = "+M+" \nClé publique = ("+N+","+C+")\nClé Privé = ("+N+", "+U+")\n";
 	}
 
 }
-
 
