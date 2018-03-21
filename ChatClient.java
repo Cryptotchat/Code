5ea52ea6c376d5e31d1652c11ecfa2adc86dd0fc
Krocorentin <35558221+Krocorentin@users.noreply.github.com>
2018-03-21 13:51:44 +0100

    Update ChatClient.java

--- ChatClient.java
+++ ChatClient.java
@@ -1,37 +1,34 @@
-import java.rmi.RemoteException;
+import java.math.BigInteger;
+import java.rmi.*;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.Vector;
+ 
+public class ChatClient  extends UnicastRemoteObject implements ChatClientInt{
+	
+	private static final long serialVersionUID = 1L;
+	private String name = " ";
+	private ChatUI ui;	
+	private Vector listecontac;
+	public ChatClient (String n) throws RemoteException {
+		name=n;
+		}
+	
+	public void tell(String st) throws RemoteException{
+		System.out.println(st);
+		ui.writeMsg(st);
+	}
+	public String getName() throws RemoteException{
+		return name;
+	}
+	
+	public void setGUI(ChatUI t){ 
+		ui=t ; 
+	}
+	
+	public void recupereContact() {
+		
+	}
 
-public class ChatClient extends RemoteException implements ClientInt, Runnable {
 
-    public final static String NDC = "utilisateur";
-    public final static String MDP = "1234";
-    private String ndc;
-    private String mdp;
-    private Vector<ChatClient> listeamis;
-
-    public ChatClient(){
-        this.ndc = NDC;
-        this.mdp = MDP;
-    }
-
-    @Override
-    public void creationCompte(String ndc, String mdp) throws RemoteException {
-        this.ndc = ndc;
-        this.mdp = mdp;
-    }
-
-    @Override
-    public String message(String message) throws RemoteException {
-        return message;
-    }
-
-    @Override
-    public void ajouter(ChatClient a) throws RemoteException {
-        listeamis.add(a);
-    }
-
-    @Override
-    public void run() {
-
-    }
 }
+
