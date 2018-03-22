f4896c1c095a1df8cd6e5d50f55bf9e4a196090c
Krocorentin <35558221+Krocorentin@users.noreply.github.com>
2018-03-22 10:26:00 +0100

    Create StartServer.java

---
+++ StartServer.java
@@ -0,0 +1,17 @@
+import java.rmi.*;
+
+public class StartServer {
+	public static void main(String[] args) {
+		try {
+				java.rmi.registry.LocateRegistry.createRegistry(10000);
+			 	System.out.println( "--");
+				ChatServerInt b=new ChatServer();
+			 	System.out.println( "--");
+				Naming.rebind("rmi://127.0.0.1:10000/myabc", b);
+			 	System.out.println( "--");
+				System.out.println("[System] pret");
+			}catch (Exception e) {
+					System.out.println(e);
+			}
+	}
+}
